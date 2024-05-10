7b9557802c81d53ddf9ea5ec3dcb255c42c8b9b1
mustafa9501 <mustafa.zaheer9501@gmail.com>
2024-05-10 16:07:44 +0530

    code

--- src/login_signup/Login.js
+++ src/login_signup/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
     const [error, setError] = useState('');
 
     const navigate = useNavigate();
-    const { signInUser, onNameHandler, onEmailHandler } = useUser();
+    const { signInUser, onNameHandler, onEmailHandler, darkTheme } = useUser();
 
     const openPopup = (event) => {
         event.preventDefault();
@@ -67,9 +67,9 @@ const Login = () => {
     return (
         <>
             {isScreenSmall ? (<>
-                <div className='bg-[#FFFBE2] py-2 pl-4 flex gap-3'>
-                    <Icon icon="fa:mobile" width="1.2rem" height="1.4rem" style={{ color: 'black' }} className='mt-1' />
-                    <p className='pt-1 text-[#3B5998]'>Get Facebook for Android and browser faster</p>
+                <div className='bg-[#FFFBE2] dark:bg-[#18191A] py-2 pl-4 flex gap-3'>
+                    <Icon icon="fa:mobile" width="1.2rem" height="1.4rem" style={{ color: darkTheme ? 'white' : 'black' }} className='mt-1' />
+                    <p className='pt-1 text-[#3B5998] dark:text-blue-600'>Get Facebook for Android and browser faster</p>
                 </div>
                 <div className='flex justify-center pt-10'>
                     <img src={Facebook} className='h-14 w-28 m-3 ml-5' />
@@ -78,19 +78,19 @@ const Login = () => {
                 <div className='w-full px-4 '>
                     <div className='text-red-700 ml-2.5 mt-1'>{error}</div>
                     <form className='flex flex-col pl-3 pr-3  text-lg' onSubmit={onClickHandler}>
-                        <input className='p-3 border border-zinc-300 rounded-md mt-1 focus:border-blue-500 focus:outline-none bg-[#F5F6F7]'
+                        <input className='p-3 border border-zinc-300 rounded-md mt-1 dark:bg-[#323436] focus:border-blue-500 focus:outline-none bg-[#F5F6F7]'
                             type='text'
                             placeholder='Enter your email'
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
                         />
-                        <input className='p-3 border border-zinc-300 rounded-md mt-3 focus:border-blue-500 focus:outline-none bg-[#F5F6F7]'
+                        <input className='p-3 border dark:bg-[#323436] border-zinc-300 rounded-md mt-3 focus:border-blue-500 focus:outline-none bg-[#F5F6F7]'
                             type='password'
                             placeholder='Enter your password'
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                         />
-                        <button type='submit' className='rounded-md mt-4 py-2.5 text-white font-bold text-xl text-semibold border bg-blue-600 cursor-pointer'>Log in</button>
+                        <button type='submit' className='rounded-md mt-4 py-2.5 text-white font-bold text-xl text-semibold border bg-blue-600 cursor-not-allowed'>Log in</button>
                         <div className='text-center mt-3 text-blue-600 text-base cursor-not-allowed'>
                             Forgotten password?
                         </div>
@@ -98,47 +98,48 @@ const Login = () => {
                         </div>
                         <div className='flex justify-center'>
                         <Link to='/signup'>
-                            <button className='px-10 py-2.5 border border-gray-300 text-black rounded-md mt-6 cursor-pointer'
+                            <button className='px-10 py-2.5 border dark:bg-[#323436] border-gray-300 dark:text-white text-black rounded-md mt-6 cursor-pointer'
                             >Create new account</button>
                         </Link>
                         </div>
                     </form>
                 </div>
                 <div className='flex justify-between px-14 pt-10'>
-                    <div className='text-[12px]'>
+                    <div className='text-[12px] dark:text-white'>
                         <h4>English (UK)</h4>
                         <h4>اردو</h4>
                         <h4>বাংলা</h4>
                         <h4>मराठी</h4>
                     </div>
-                    <div className='text-[10px]'>
+                    <div className='text-[10px] dark:text-white'>
                         <h4>हिन्दी</h4>
                         <h4>ਪੰਜਾਬੀ</h4>
                         <h4>ગુજરાતી</h4>
                         <h4>मराठी</h4>
                     </div>
                 </div>
-                <div className='text-center pt-6 text-[12px]'>Meta @ 2024</div>
+                <div className='text-center pt-6 text-[12px] dark:text-white'>Meta @ 2024</div>
             </>
             ) : (
+
                 //for large device login code
-                <div className='bg-[#F0F2F5] w-full h-full flex justify-center items-center gap-1'>
+                <div className='bg-[#F0F2F5] dark:bg-[#18191A] dark:text-white w-full h-full flex justify-center items-center gap-1'>
                     <div className='w-1/2 px-8'>
                         <div className='text-6xl font-bold text-[#0866FF]'>facebook</div>
-                        <div className='text-3xl mt-3'>Facebook helps you connect and share
+                        <div className='text-3xl mt-3 text-black dark:text-white'>Facebook helps you connect and share
                             with the people in your life.
                         </div>
                     </div>
-                    <div className='bg-white w-2/7 h-3/7 rounded-lg drop-shadow-md'>
+                    <div className='bg-white dark:bg-[#242526] w-2/7 h-3/7 rounded-lg drop-shadow-md'>
                         <div className='text-red-700 ml-2.5 mt-1'>{error}</div>
                         <form className='flex flex-col pl-3 pr-3 pt-2 text-lg' onSubmit={onClickHandler}>
-                            <input className='p-3 border border-zinc-300 rounded-md mt-1 focus:border-blue-500 focus:outline-none'
+                            <input className='p-3 border border-zinc-300 dark:bg-[#323436] rounded-md mt-1 focus:border-blue-500 focus:outline-none'
                                 type='text'
                                 placeholder='Enter your email'
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
                             />
-                            <input className='p-3 border border-zinc-300 rounded-md mt-3 focus:border-blue-500 focus:outline-none'
+                            <input className='p-3 border border-zinc-300 dark:bg-[#323436] rounded-md mt-3 focus:border-blue-500 focus:outline-none'
                                 type='password'
                                 placeholder='Enter your password'
                                 value={password}
@@ -148,7 +149,7 @@ const Login = () => {
                             <div className='text-center mt-3 text-blue-600 text-base cursor-not-allowed'>
                                 Forgotten password?
                             </div>
-                            <div className='border-b border-zinc-200 mt-5'>
+                            <div className='border-b border-zinc-200 dark:border-zinc-500 mt-5'>
                             </div>
                             <button className='px-2 py-2.5 border bg-[#42B72A] rounded-md w-1/2 m-auto mt-6 font-semibold text-white cursor-pointer'
                                 onClick={openPopup}>Create new account</button>
--- src/login_signup/Signup.js
+++ src/login_signup/Signup.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Icon } from '@iconify/react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { useUser } from '../provider/UserProvider';
 
 const Signup = ({ onClose }) => {
 
@@ -9,6 +10,7 @@ const Signup = ({ onClose }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const {darkTheme} = useUser();
 
   const navigate = useNavigate();
 
@@ -47,12 +49,12 @@ const Signup = ({ onClose }) => {
     <>
       {isScreenSmall ? (
         <>  
-        <div className="h-screen w-screen flex items-center justify-center bg-white px-6">
-          <div className="mian_popup bg-white rounded-lg drop-shadow-lg pb-4">
+        <div className="h-screen w-screen flex items-center justify-center dark:bg-[#18191A] bg-white px-6">
+          <div className="mian_popup bg-white dark:bg-[#242526] rounded-lg drop-shadow-lg pb-4">
             <div className='flex justify-between'>
               <div className='block p-3'>
-                <div className='text-4xl font-semibold'>Sign Up</div>
-                <div className='test-sm mt-1.5 text-zinc-600'>It's quick and easy.</div></div>
+                <div className='text-4xl font-semibold dark:text-white'>Sign Up</div>
+                <div className='test-sm mt-1.5 text-zinc-600 dark:text-white'>It's quick and easy.</div></div>
               <div className='cursor-pointer pr-3 pt-3' >
                 <Icon icon="maki:cross" width="1.4rem" height="1.4rem" style={{ color: '#606771' }} onClick={()=>navigate(-1)}/>
               </div>
@@ -61,26 +63,26 @@ const Signup = ({ onClose }) => {
             <idv className='text-red-700 pl-3'>{error}</idv>
             <div className=''>
               <form className='flex flex-col pl-3 pr-3 pt-1 text-lg' onSubmit={onClickHandler}>
-                <input className='p-2 border border-zinc-300 rounded-md mt-1 focus:border-blue-500 focus:outline-none'
+                <input className='p-2 border border-zinc-300 dark:bg-[#323436] dark:text-white rounded-md mt-1 focus:border-blue-500 focus:outline-none'
                   type='text'
                   placeholder='Enter your full name'
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                 />
-                <input className='p-2 border border-zinc-300 rounded-md mt-2 focus:border-blue-500 focus:outline-none'
+                <input className='p-2 border border-zinc-300 dark:bg-[#323436] dark:text-white rounded-md mt-2 focus:border-blue-500 focus:outline-none'
                   type='text'
                   placeholder='Enter your email'
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
-                <input className='p-2 border border-zinc-300 rounded-md mt-2 focus:border-blue-500 focus:outline-none'
+                <input className='p-2 border border-zinc-300 dark:bg-[#323436] dark:text-white rounded-md mt-2 focus:border-blue-500 focus:outline-none'
                   type='password'
                   placeholder='Create password'
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
-                <div className='text-xs text-zinc-700 mt-4'>People who use our service may have uploaded your contact information to Facebook. <span className='text-blue-800 hover:underline cursor-pointer'>Learn more</span>.</div>
-                <div className='text-xs text-zinc-700 mt-3'>By clicking Sign Up, you agree to our <span className='text-blue-900 hover:underline cursor-pointer'>Terms</span>, <span className='text-blue-900 hover:underline cursor-pointer'>Privacy Policy</span> and <span className='text-blue-900 hover:underline cursor-pointer'>Cookies Policy</span>. You may receive SMS notifications from us and can opt out at any time.</div>
+                <div className='text-xs text-zinc-700 mt-4 dark:text-white'>People who use our service may have uploaded your contact information to Facebook. <span className='text-blue-800 dark:text-blue-600 hover:underline cursor-pointer'>Learn more</span>.</div>
+                <div className='text-xs text-zinc-700 mt-3 dark:text-white'>By clicking Sign Up, you agree to our <span className='text-blue-900 dark:text-blue-600 hover:underline cursor-pointer'>Terms</span>, <span className='text-blue-900 hover:underline cursor-pointer dark:text-blue-600'>Privacy Policy</span> and <span className='text-blue-900 hover:underline cursor-pointer dark:text-blue-600'>Cookies Policy</span>. You may receive SMS notifications from us and can opt out at any time.</div>
                 <button type='submit' className='rounded-md mt-4 w-3/7 py-1 m-auto text-white font-bold text-xl text-semibold border bg-[#00A400] cursor-pointer' onClick={onClickHandler}>Sign Up</button>
               </form>
             </div>
@@ -90,12 +92,12 @@ const Signup = ({ onClose }) => {
 
       ) : (
 
-        <div className="fixed h-screen w-screen flex items-center justify-center bg-white bg-opacity-80">
-          <div className="mian_popup bg-white w-2/7 h-4/6 rounded-lg drop-shadow-lg">
+        <div className="fixed h-screen w-screen flex items-center justify-center bg-white dark:bg-[#18191A] dark:bg-opacity-80 bg-opacity-80">
+          <div className="mian_popup bg-white dark:bg-[#242526] w-2/7 h-4/6 rounded-lg drop-shadow-lg">
             <div className='flex justify-between'>
               <div className='block p-3'>
-                <div className='text-4xl font-semibold'>Sign Up</div>
-                <div className='test-sm mt-1.5 text-zinc-600'>It's quick and easy.</div></div>
+                <div className='text-4xl font-semibold text-black dark:text-white'>Sign Up</div>
+                <div className='test-sm mt-1.5 text-zinc-600 dark:text-white'>It's quick and easy.</div></div>
               <div className='cursor-pointer pr-3 pt-3' onClick={onClose}>
                 <Icon icon="maki:cross" width="1.4rem" height="1.4rem" style={{ color: '#606771' }} />
               </div>
@@ -104,26 +106,26 @@ const Signup = ({ onClose }) => {
             <idv className='text-red-700 pl-3'>{error}</idv>
             <div className=''>
               <form className='flex flex-col pl-3 pr-3 pt-1 text-lg' onSubmit={onClickHandler}>
-                <input className='p-2 border border-zinc-300 rounded-md mt-1 focus:border-blue-500 focus:outline-none'
+                <input className='p-2 border border-zinc-300 rounded-md mt-1 dark:bg-[#323436] focus:border-blue-500 focus:outline-none'
                   type='text'
                   placeholder='Enter your full name'
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                 />
-                <input className='p-2 border border-zinc-300 rounded-md mt-2 focus:border-blue-500 focus:outline-none'
+                <input className='p-2 border border-zinc-300 rounded-md mt-2 dark:bg-[#323436] focus:border-blue-500 focus:outline-none'
                   type='text'
                   placeholder='Enter your email'
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
-                <input className='p-2 border border-zinc-300 rounded-md mt-2 focus:border-blue-500 focus:outline-none'
+                <input className='p-2 border border-zinc-300 rounded-md mt-2 dark:bg-[#323436] focus:border-blue-500 focus:outline-none'
                   type='password'
                   placeholder='Create password'
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
-                <div className='text-xs text-zinc-700 mt-4'>People who use our service may have uploaded your contact information to Facebook. <span className='text-blue-800 hover:underline cursor-pointer'>Learn more</span>.</div>
-                <div className='text-xs text-zinc-700 mt-3'>By clicking Sign Up, you agree to our <span className='text-blue-900 hover:underline cursor-pointer'>Terms</span>, <span className='text-blue-900 hover:underline cursor-pointer'>Privacy Policy</span> and <span className='text-blue-900 hover:underline cursor-pointer'>Cookies Policy</span>. You may receive SMS notifications from us and can opt out at any time.</div>
+                <div className='text-xs text-zinc-700 mt-4 dark:text-white'>People who use our service may have uploaded your contact information to Facebook. <span className='text-blue-800 hover:underline cursor-pointer'>Learn more</span>.</div>
+                <div className='text-xs text-zinc-700 mt-3 dark:text-white'>By clicking Sign Up, you agree to our <span className='text-blue-900 hover:underline cursor-pointer'>Terms</span>, <span className='text-blue-900 hover:underline cursor-pointer'>Privacy Policy</span> and <span className='text-blue-900 hover:underline cursor-pointer'>Cookies Policy</span>. You may receive SMS notifications from us and can opt out at any time.</div>
                 <button type='submit' className='rounded-md mt-4 w-3/7 py-1 m-auto text-white font-bold text-xl text-semibold border bg-[#00A400] cursor-pointer' onClick={onClickHandler}>Sign Up</button>
               </form>
             </div>
